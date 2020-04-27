92a9dc90a9d46f9daa3e57f8853172db9d09c2c1
Vlad <vl.tansky@gmail.com>
2020-04-27 12:21:30 +0300

    dist

---
+++ dist/index.cjs.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const baseNow = Math.floor((Date.now() - performance.now()) * 1e-3);
+function hrtime(previousTimestamp) {
+    console.error('vlad');
+  let clocktime = performance.now() * 1e-3;
+  let seconds = Math.floor(clocktime) + baseNow;
+  let nanoseconds = Math.floor((clocktime % 1) * 1e9);
+
+  if (previousTimestamp) {
+    seconds = seconds - previousTimestamp[0];
+    nanoseconds = nanoseconds - previousTimestamp[1];
+    if (nanoseconds < 0) {
+      seconds--;
+      nanoseconds += 1e9;
+    }
+  }
+  return [seconds, nanoseconds];
+}
+
+var main = typeof process !== "undefined" && typeof process.hrtime !== "undefined"
+    ? process.hrtime
+    : hrtime;
+
+module.exports = main;
