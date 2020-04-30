bc918a813ee7e68ceac1dc648fa657e63b0bbbb3
Vlad <vl.tansky@gmail.com>
2020-04-30 16:04:10 +0300

    tests: ES6

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    server: {
+      command: 'http-server -c-1 -p 1234',
+    },
     launch: {
         headless: process.env.HEADLESS !== 'false',
         slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
--- test/browser.test.js
+++ test/browser.test.js
@@ -1,69 +1,95 @@
-const pti = require('puppeteer-to-istanbul')
+const pti = require('puppeteer-to-istanbul');
 const puppeteer = require('puppeteer');
 const path = require('path');
 
 const timeout = process.env.SLOWMO ? 30000 : 10000;
 let browser, page;
 
-const open_page = async (page, name) => {
-  return await page.goto(`file:${path.join(__dirname, name + '.html')}`, { waitUntil: 'domcontentloaded' });
-};
-
 describe('browser', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
+    await page.goto(`http://127.0.0.1:1234/test/'`, { waitUntil: 'domcontentloaded' });
     await page.coverage.startJSCoverage();
-    await open_page(page, 'umd');
   });
-  // describe('process.hrtime.bigint()', () => {
-  //   test('process.hrtime.bigint() should be a function', async () => {
-  //     const hrtimeType = await page.evaluate(() => {
-  //       return typeof process.hrtime.bigint;
-  //     });
-  //     expect(hrtimeType).toBe('function');
-  //   }, timeout);
 
-  //   test('global var hrtime.bigint() should be a function', async () => {
-  //     const hrtimeType = await page.evaluate(() => {
-  //       return typeof hrtime.bigint;
-  //     });
-  //     expect(hrtimeType).toBe('function');
-  //   }, timeout);
+  beforeAll(async () => {
+    await page.addScriptTag({ url: '../lib/hrtime.js', type: 'module' });
+  });
+  describe('process.hrtime.bigint()', () => {
+    test(
+      'process.hrtime.bigint() should be a function',
+      async () => {
+        const hrtimeType = await page.evaluate(() => {
+          return typeof process.hrtime.bigint;
+        });
+        expect(hrtimeType).toBe('function');
+      },
+      timeout
+    );
 
-  //   test('process.hrtime.bigint() should return number', async () => {
-  //     const result = await page.evaluate(() => {
-  //       var time = process.hrtime.bigint(); // e.g [ 1800216, 25 ]
-  //       return time;
-  //     });
-  //   expect(result).toBeGreaterThanOrEqual(0);
-  // }, timeout);
-  // });
+    test(
+      'global var hrtime.bigint() should be a function',
+      async () => {
+        const hrtimeType = await page.evaluate(() => {
+          return typeof hrtime.bigint;
+        });
+        expect(hrtimeType).toBe('function');
+      },
+      timeout
+    );
+
+    test(
+      'process.hrtime.bigint() should return number',
+      async () => {
+        const result = await page.evaluate(() => {
+          var time = process.hrtime.bigint(); // e.g [ 1800216, 25 ]
+          return time;
+        });
+        expect(result).toBeGreaterThanOrEqual(0);
+      },
+      timeout
+    );
+  });
 
   describe('process.hrtime()', () => {
-    test('process.hrtime() should be a function', async () => {
-      const hrtimeType = await page.evaluate(() => {
-        return typeof process.hrtime;
-      });
-      expect(hrtimeType).toBe('function');
-    }, timeout);
+    test(
+      'process.hrtime() should be a function',
+      async () => {
+        const hrtimeType = await page.evaluate(() => {
+          return typeof process.hrtime;
+        });
+        expect(hrtimeType).toBe('function');
+      },
+      timeout
+    );
 
-    test('global var hrtime() should be a function', async () => {
-      const hrtimeType = await page.evaluate(() => {
-        return typeof hrtime;
-      });
-      expect(hrtimeType).toBe('function');
-    }, timeout);
+    test(
+      'global var hrtime() should be a function',
+      async () => {
+        const hrtimeType = await page.evaluate(() => {
+          return typeof hrtime;
+        });
+        expect(hrtimeType).toBe('function');
+      },
+      timeout
+    );
 
-    test('process.hrtime() should return array length of 2', async () => {
+    test(
+      'process.hrtime() should return array length of 2',
+      async () => {
         const result = await page.evaluate(() => {
           var time = process.hrtime(); // e.g [ 1800216, 25 ]
           return time;
         });
-      expect(result).toHaveLength(2);
-    }, timeout);
+        expect(result).toHaveLength(2);
+      },
+      timeout
+    );
 
-    test('should return around ~1000000552 nanoseconds', async () => {
+    test(
+      'should return around ~1000000552 nanoseconds',
+      async () => {
         const result = await page.evaluate(() => {
           return new Promise((resolve, reject) => {
             var time = process.hrtime(); // e.g [ 1800216, 25 ]
@@ -74,7 +100,7 @@ describe('browser', () => {
           });
         });
         const actual = Math.round(result / 100000000);
-        expect(actual).toBe(10);// 1000000552 nanoseconds
+        expect(actual).toBe(10); // 1000000552 nanoseconds
       },
       timeout
     );
